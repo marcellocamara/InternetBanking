f535bc781f5aaef64c7ad6f8b973ed77aafe77bc
Marcello Câmara <marcellocamara@id.uff.br>
2021-06-04 15:36:17 -0300

    feat: repository provider on main app with dependency injection on settings bloc

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -5,22 +5,25 @@ import 'app.routes.dart';
 import 'app.theme.dart';
 import 'features/settings/settings.bloc.dart';
 import 'features/settings/settings.state.dart';
+import 'shared/repositories/local_storage/local_storage.repo.dart';
 
 /// Created by marcellocamara@id.uff.br on 02/06/2021.
 
 class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) => SettingsBloc(SettingsState()),
+    return RepositoryProvider<LocalStorageRepository>(
+      create: (_) => LocalStorageRepository(),
+      child: BlocProvider<SettingsBloc>(
+        create: (context) => SettingsBloc(
+          initialState: SettingsState(),
+          localStorageRepository: context.read<LocalStorageRepository>(),
         ),
-      ],
-      child: MaterialApp(
-        title: 'Internet Banking Simulator',
-        theme: AppTheme.themeData,
-        onGenerateRoute: AppRoutes.onGenerateRoute,
+        child: MaterialApp(
+          title: 'Internet Banking Simulator',
+          theme: AppTheme.themeData,
+          onGenerateRoute: AppRoutes.onGenerateRoute,
+        ),
       ),
     );
   }
