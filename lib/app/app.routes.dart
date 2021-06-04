8ba8a949cdac3f1ac79567eb2fd2d5b8da42627b
Marcello Câmara <marcellocamara@id.uff.br>
2021-06-04 15:35:48 -0300

    feat: add splash page on app routes addresses

--- lib/app/app.routes.dart
+++ lib/app/app.routes.dart
@@ -4,18 +4,22 @@ import 'package:flutter/material.dart';
 import 'features/bank1/bank1.page.dart';
 import 'features/bank2/bank2.page.dart';
 import 'features/settings/settings.page.dart';
+import 'features/splash/splash.page.dart';
 
 /// Created by marcellocamara@id.uff.br on 03/06/2021.
 
 abstract class AppRoutes {
-  static const settings = '/';
+  static const splash = '/';
+  static const settings = '/settings';
   static const bank1 = '/bank1';
   static const bank2 = '/bank2';
 
   static Route onGenerateRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
+      case splash:
+        return MaterialPageRoute(builder: (_) => SplashPage());
       case settings:
-        return MaterialPageRoute(builder: (_) => SettingsPage());
+        return CupertinoPageRoute(builder: (_) => SettingsPage());
       case bank1:
         return CupertinoPageRoute(builder: (_) => Bank1Page());
       case bank2:
