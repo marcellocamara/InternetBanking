ba58692735a41cd4eefbdc3cfe7ebf1e4c004d5c
Marcello Câmara <marcellocamara@id.uff.br>
2021-06-03 23:03:36 -0300

    refactor: main material app routes and theme configuration

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,14 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'app.routes.dart';
+import 'app.theme.dart';
 import 'features/settings/settings.bloc.dart';
 import 'features/settings/settings.state.dart';
 
 /// Created by marcellocamara@id.uff.br on 02/06/2021.
 
 class MainApp extends StatelessWidget {
-  final _appRoutes = AppRoutes();
-
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
@@ -20,10 +19,8 @@ class MainApp extends StatelessWidget {
       ],
       child: MaterialApp(
         title: 'Internet Banking Simulator',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        onGenerateRoute: _appRoutes.onGenerateRoute,
+        theme: AppTheme.themeData,
+        onGenerateRoute: AppRoutes.onGenerateRoute,
       ),
     );
   }
