5b962ca68db51914aee463b2ecc4902e6035dd44
Marcello Câmara <marcellocamara@id.uff.br>
2021-06-04 18:00:25 -0300

    feat: app icon asset file and asset path

--- lib/app/shared/theme/app.assets.dart
+++ lib/app/shared/theme/app.assets.dart
@@ -1,6 +1,9 @@
 /// Created by marcellocamara@id.uff.br on 04/06/2021.
 
 abstract class AppAssets {
+  /// Icons - SVG
+  static const icon_coin = 'assets/icons/icon_coin.json';
+
   /// Animations - JSON
   static const anim_coin = 'assets/animations/anim_coin.json';
 }
