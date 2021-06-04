f19174311dadc5c317e52b9359ce7cde6551e0ab
Marcello Câmara <marcellocamara@id.uff.br>
2021-06-04 11:30:18 -0300

    feat: added the bank1 and bank2 temporary pages into app routes

--- lib/app/app.routes.dart
+++ lib/app/app.routes.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'features/bank1/bank1.page.dart';
+import 'features/bank2/bank2.page.dart';
 import 'features/settings/settings.page.dart';
 
 /// Created by marcellocamara@id.uff.br on 03/06/2021.
 
 abstract class AppRoutes {
+  static const settings = '/';
+  static const bank1 = '/bank1';
+  static const bank2 = '/bank2';
+
   static Route onGenerateRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
-      case '/':
-        return CupertinoPageRoute(
-          builder: (_) => SettingsPage(),
-        );
+      case settings:
+        return MaterialPageRoute(builder: (_) => SettingsPage());
+      case bank1:
+        return CupertinoPageRoute(builder: (_) => Bank1Page());
+      case bank2:
+        return CupertinoPageRoute(builder: (_) => Bank2Page());
       default:
         return null;
     }
