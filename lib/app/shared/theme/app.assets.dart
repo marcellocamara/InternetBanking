d24ae3101490d3ab8e466e21275e1ef8c46bcbe8
Marcello Câmara <marcellocamara@id.uff.br>
2021-06-05 16:28:20 -0300

    feat: santander asset icons

--- lib/app/shared/theme/app.assets.dart
+++ lib/app/shared/theme/app.assets.dart
@@ -2,7 +2,17 @@
 
 abstract class AppAssets {
   /// Icons - SVG
-  static const icon_coin = 'assets/icons/icon_coin.json';
+  static const icon_coin = 'assets/icons/icon_coin.svg';
+  static const icon_santander = 'assets/icons/icon_santander.svg';
+  static const icon_santander_menu = 'assets/icons/icon_santander_menu.svg';
+  static const icon_santander_bell = 'assets/icons/icon_santander_bell.svg';
+  static const icon_santander_balance = 'assets/icons/icon_santander_balance.svg';
+  static const icon_santander_arrow_up = 'assets/icons/icon_santander_arrow_up.svg';
+  static const icon_santander_pix = 'assets/icons/icon_santander_pix.svg';
+  static const icon_santander_barcode = 'assets/icons/icon_santander_barcode.svg';
+  static const icon_santander_send = 'assets/icons/icon_santander_send.svg';
+  static const icon_santander_receive = 'assets/icons/icon_santander_receive.svg';
+  static const icon_santander_proof = 'assets/icons/icon_santander_proof.svg';
 
   /// Animations - JSON
   static const anim_coin = 'assets/animations/anim_coin.json';
